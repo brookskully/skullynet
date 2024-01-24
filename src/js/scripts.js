72fdddd8a1a97f09e277fb04e562a8c62d1bffba
brookskully <info@brookskully.com>
2024-01-24 23:41:57 +0100

    intentando angular

---
+++ src/js/scripts.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC90nuYcCKjBpITYjAWvZqqIs9k9v0zVjQ",
+  authDomain: "skullynet.firebaseapp.com",
+  databaseURL: "https://skullynet-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "skullynet",
+  storageBucket: "skullynet.appspot.com",
+  messagingSenderId: "686579029011",
+  appId: "1:686579029011:web:62267426842ac316235be6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
