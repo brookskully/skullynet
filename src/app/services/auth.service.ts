4bb04c781885457779dc1c476ccf1eacbdfe94aa
brookskully <info@brookskully.com>
2024-01-26 13:45:57 +0100

    Auth methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { 
+  Auth, 
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+ } from '@angular/fire/auth'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private auth: Auth) {}
 
-  constructor() { }
+   async register(email: any, password: any){
+    
+    createUserWithEmailAndPassword(this.auth, email, password);
+    try{
+      const user = await createUserWithEmailAndPassword(
+        this.auth, 
+        email, 
+        password
+      );
+      return user;
+    }catch(e){
+       { return null }
+    }
+   }
+
+   async login(email: any, password: any){
+    try{
+      const user = await signInWithEmailAndPassword(
+        this.auth, 
+        email, 
+        password
+      );
+      return user;
+    }catch(e){
+       { return null }
+    }
+   }
+
+   logout(){
+    return signOut(this.auth);
+   }
+
 }
